1f60ff9147178846c0baa509e5561d9a3ab10ec2
spidΞy <web3slinger@gmail.com>
2022-06-15 14:14:44 -0500

    Build ether coffee contract

--- hardhat/contracts/EtherCoffee.sol
+++ hardhat/contracts/EtherCoffee.sol
@@ -3,14 +3,43 @@ pragma solidity ^0.8.7;
 
 import "hardhat/console.sol";
 
-contract EtherCoffee {
-    uint256 private constant PRICE = 1000000000000000000;
+error EtherCoffee__PriceMustBeAboveZero();
+error EtherCoffee__NoProceeds();
 
-    constructor() {
-        console.log("\n gm");
+contract EtherCoffee {
+    mapping(address => uint256) private s_proceeds;
+
+    event CoffeeBought(
+        address indexed sender,
+        uint256 indexed amount,
+        uint256 indexed timestamp,
+        string name,
+        string message
+    );
+
+    function buyCoffee(
+        address _user,
+        string memory _name,
+        string memory _message
+    ) public payable {
+        if (msg.value <= 0) {
+            revert EtherCoffee__PriceMustBeAboveZero();
+        }
+        s_proceeds[_user] += msg.value;
+        emit CoffeeBought(msg.sender, msg.value, block.timestamp, _name, _message);
     }
 
-    function getPrice() public pure returns (uint256) {
-        return PRICE;
+    function withdrawProceeds() external {
+        uint256 proceeds = s_proceeds[msg.sender];
+        if (proceeds <= 0) {
+            revert EtherCoffee__NoProceeds();
+        }
+        s_proceeds[msg.sender] = 0;
+        (bool success, ) = payable(msg.sender).call{value: proceeds}("");
+        require(success, "Transfer failed");
+    }
+
+    function getProceeds(address user) external view returns (uint256) {
+        return s_proceeds[user];
     }
 }
