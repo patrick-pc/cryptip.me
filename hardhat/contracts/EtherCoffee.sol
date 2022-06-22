78cafd8996be9327a3467e1c7695de84cfaca4c6
spidΞy <web3slinger@gmail.com>
2022-06-21 20:31:56 -0500

    Add receiver address in coffee bought event

--- hardhat/contracts/EtherCoffee.sol
+++ hardhat/contracts/EtherCoffee.sol
@@ -13,15 +13,17 @@ contract EtherCoffee {
 
     /// @notice This event is emitted when the contract receives an ether
     /// @dev Using events to get the sender message and details using an indexing protocol
+    /// @param receiver Receiver address
     /// @param sender Sender address
     /// @param amount The amount
     /// @param timestamp Transaction timestamp
     /// @param name Name of the sender
     /// @param message Message from the sender
     event CoffeeBought(
+        address indexed receiver,
         address indexed sender,
         uint256 indexed amount,
-        uint256 indexed timestamp,
+        uint256 timestamp,
         string name,
         string message
     );
@@ -36,21 +38,19 @@ contract EtherCoffee {
         string memory name,
         string memory message
     ) public payable {
-        if (msg.value <= 0) {
-            revert EtherCoffee__ValueMustBeAboveZero();
-        }
+        if (msg.value <= 0) revert EtherCoffee__ValueMustBeAboveZero();
+
         // Could just send the money...
         // https://fravoll.github.io/solidity-patterns/pull_over_push.html
         s_proceeds[userAddress] += msg.value;
-        emit CoffeeBought(msg.sender, msg.value, block.timestamp, name, message);
+        emit CoffeeBought(userAddress, msg.sender, msg.value, block.timestamp, name, message);
     }
 
     /// @notice Gets the amount that an address have
     function withdrawProceeds() external {
         uint256 proceeds = s_proceeds[msg.sender];
-        if (proceeds <= 0) {
-            revert EtherCoffee__NoProceeds();
-        }
+        if (proceeds <= 0) revert EtherCoffee__NoProceeds();
+
         s_proceeds[msg.sender] = 0;
         (bool success, ) = payable(msg.sender).call{value: proceeds}("");
         require(success, "Transfer failed");
